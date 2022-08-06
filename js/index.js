8562abe58516c72656015ac244a648a998d6bf3a
LilyEnz <97416783+LilyEnz@users.noreply.github.com>
2022-08-05 23:24:12 -0500

    modal images!

--- js/index.js
+++ js/index.js
@@ -1,1 +1,70 @@
 console.log("Your index.js file is loaded correctly!");
+
+var modal = document.getElementById("bigFlockApp");
+
+var img = document.getElementById("FlockApp");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
+var modal = document.getElementById("bigMSPparkingApp");
+
+var img = document.getElementById("mspParkingApp");
+var modalImg = document.getElementById("img02");
+var captionText = document.getElementById("caption");
+
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
+var modal = document.getElementById("bigNOAA");
+
+var img = document.getElementById("NOAA");
+var modalImg = document.getElementById("img03");
+var captionText = document.getElementById("caption");
+
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
+
+/** $("workImg").hover(
+    function() {
+    $(this).addClass("enlargeWorkImg");
+}, function() {
+    $(this).removeClass("enlargeWorkImg");
+}
+); */
